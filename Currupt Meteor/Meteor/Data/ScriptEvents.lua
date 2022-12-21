10243d20f1fe8abb7f2e778c5f5c629a91e037b5
SkIlLIsSuEs <103995093+Millerrrrrrrrrr@users.noreply.github.com>
2022-12-21 02:59:44 +0000

    Add files via upload

---
+++ Currupt Meteor/Meteor/Data/ScriptEvents.lua
@@ -0,0 +1,10 @@
+return {
+	["Kick 1"] = 1268038438,
+	["Breakup 1"] = 915462795,
+	["Infinite Loop"] = -904555865,
+	["Script Teleport"] = 879177392,
+	["Give Collectibles"] = 697566862,
+	["Interior Invite"] = -1891171016,
+	["Vehicle Kick"] = -852914485,
+	["Transaction Error"] = -492741651
+}
